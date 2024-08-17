647df10ce814cb21e6954ff2b5f7b9cda9ecfa78
heisenbergO312 <heisenberg.03123@gmail.com>
2024-08-17 12:10:25 +0530

    changes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -128,7 +128,7 @@ const App = () => {
     const username = result.value;
     setPlayerName(username);
 
-    const newSocket = io("http://localhost:3000", {
+    const newSocket = io("https://tictactoeonline-7hyo.onrender.com", {
       autoConnect: true,
     });
 
